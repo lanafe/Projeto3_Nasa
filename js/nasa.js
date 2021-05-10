ec57e16e0f740d97f666ef296bb579dfcc4a5c75
lanafe <lanafe99@gmail.com>
2021-05-10 18:40:44 -0300

    Correção do projeto 3

--- js/nasa.js
+++ js/nasa.js
@@ -1,34 +1,11 @@
-// let buscarButton = document.querySelector("#buscar");
-//let data2 = document.querySelector("#procura");
-
-//document.addEventListener("click",()=>{
-    //console.log("button pressed");
-    //sendApiRequest();
-//})
-
-//async function sendApiRequest (){
-  //  let API_KEY = "QjZThVyVKLyDiCGWSeHMNTR3Bcih2fNwnpxphMk1"
-    //console.log(data2);
-    //let responde = await fetch("https://api.nasa.gov/planetary/apod?api_key=QjZThVyVKLyDiCGWSeHMNTR3Bcih2fNwnpxphMk1")
-    //console.log(response);
-    //let data = await responde.json();
-    //console.log(data);
-    //useApiData(data);
-//}
-
-  //  function useApiData(data){
-    //    document.querySelector("#content").innerHTML += data.explanation;
-      //  document.querySelector("#content").innerHTML += '<img src="${data.url}">';
-//}
 function buscaFotoDoDiaAPOD() {
+  let data = $('#procura').val();
     const key = 'QjZThVyVKLyDiCGWSeHMNTR3Bcih2fNwnpxphMk1'
     $.ajax({
-        url: `https://api.nasa.gov/planetary/apod?api_key=${key}`,
+        url: `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data}`,
         dataType: 'json',
         success: function(dados) {
             document.getElementById('imgApod').src = dados.url
         }
     })
 }
-
-buscaFotoDoDiaAPOD()
